aba98afb9833654f17bc2f1152b162d91cde0e80
Василий <basilfisher@mail.ru>
2024-02-24 01:58:11 +0300

    Logics of password validation is successfully completed.

--- src/main/java/ru/inno/course/homework4/Task5_6Password.java
+++ src/main/java/ru/inno/course/homework4/Task5_6Password.java
@@ -1,38 +1,80 @@
 package ru.inno.course.homework4;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Task5_6Password {
 
     private String password;
 
-    char[] specialSymbols = {'!', '@', '#', '$', '%', '^', '&', '*', '№'};
+    String regexOne = "[0-9]";
+    String regexTwo = "[!@#$%^&*№]";
+
+    boolean passwordLengthIsEigthSymbol;
+    boolean passwordContainsAtLeastOneDigit;
+    boolean passwordContainsAtLeastOneSymbol;
+    boolean finalCheck;
 
     private void setPassword(String pass) {
         this.password = pass;
     }
 
     private void passwordComparation(String pass) {
-        if (pass.equals("Qwerty0987654321")) {
+        if ((pass.equals("Qwerty0987654321")) & (finalCheck)) {
             System.out.println("Доступ разрешен");
         } else System.out.println("Доступ запрещен");
     }
 
-    private void passwordChecking(String checkPass) {
-        if (checkPass.length() < 8) {
-            System.out.println("Пароль не менее 8 символов");
-            /*Честно говоря, это единственное задание из блока, которое не выполнилось за пару минут. Я так и не приду -
-            мал, как эффективно выполнить остальные проверки. Пробовал разные варианты, но они получались громоздкими,
-            запутанными, с инициализацией дополнительных локальных переменных. Однако я уверен, что есть какое-то
-            простое и изящное решение. Буду признателен, если вы намекнёте, в каком направлении мне искать.
-            И ещё: я только вчера вечером сел вплотную изучать Git. Поэтому первая посылка файлов вышла так себе.
-            Прошу извинить за эту оплошность. Исправлюсь.
-             */
-        } else System.out.println("Пароль принят");
+    private void passwordLenghtChecking(String pass1) {
+        if (pass1.length() < 8) {
+            System.out.println("Пароль должен содержать не менее 8 символов");
+            passwordLengthIsEigthSymbol = false;
+        } else this.passwordLengthIsEigthSymbol = true;
+    }
+
+    private void passwordDigitChecking(String pass2) {
+        Pattern p1 = Pattern.compile(regexOne, Pattern.CASE_INSENSITIVE);
+        Matcher m1 = p1.matcher(pass2);
+        if(m1.find()) {
+        passwordContainsAtLeastOneDigit = true;
+        } else {
+            System.out.println("Пароль должен содержать минимум 1 цифру");
+            passwordContainsAtLeastOneDigit = false;
+        }
+    }
+
+    private void passwordSymbolChecking(String pass3) {
+        Pattern p2 = Pattern.compile(regexTwo, Pattern.CASE_INSENSITIVE);
+        Matcher m2 = p2.matcher(pass3);
+        if (m2.find()) {
+            passwordContainsAtLeastOneSymbol = true;
+        } else {
+            System.out.println("Пароль должен содержать минимум 1 спецсимвол");
+            passwordContainsAtLeastOneSymbol = false;
+        }
+    }
+
+    private void passwordOverSchecking(String p) {
+        this.passwordLenghtChecking(p);
+        this.passwordDigitChecking(p);
+        this.passwordSymbolChecking(p);
+        if(passwordLengthIsEigthSymbol & passwordContainsAtLeastOneDigit & passwordContainsAtLeastOneSymbol) {
+            System.out.println("Пароль принят");
+            this.finalCheck = true;
+        } else {
+            this.finalCheck = false;
+        }
     }
 
     public static void main(String[] args) {
+
         Task5_6Password passwordChecker = new Task5_6Password();
+
         passwordChecker.setPassword("Qwerty0987654321");
+
+        passwordChecker.passwordOverSchecking(passwordChecker.password);
+
         passwordChecker.passwordComparation(passwordChecker.password);
-        passwordChecker.passwordChecking(passwordChecker.password);
     }
 }
+
